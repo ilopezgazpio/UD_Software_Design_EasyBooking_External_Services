eebf35763c211d6c097784e19ebd350520e28435
inigolopezgazpio <inigo.lopezgazpio@deusto.es>
2020-03-31 18:25:32 +0200

    Minor changes

--- launch_DeustoAuth_micro_service.py
+++ launch_DeustoAuth_micro_service.py
@@ -49,60 +49,60 @@ class MicroServices(Resource):
 
     # curl http://127.0.0.1:5000/
     def get(self):
-        return {'Status': 'Working'}, 201
+        return {'Status' : 'Working'}, 201
 
 
 class Auth_MicroService_Log_in (Resource):
 
     # curl http://127.0.0.1:5000/Authentication/Log_in
     def get(self):
-        return {'Status': 'Working'}, 201
+        return {'Status' : 'Working'}, 201
 
     # curl http://127.0.0.1:5000/Authentication/Log_in -d '{"email":"inigo.lopezgazpio@deusto.es", "password":"XXX" }' -X POST -H "Content-Type: application/json" -v
     def post(self):
         user_args = user_parser.parse_args()
         result = deusto_auth.log_in(user_args.email, user_args.password)
-        return {'Result': result}, 201
+        return {'Result' : result}, 201
 
 
 class Auth_MicroService_Create_User (Resource):
 
     # curl http://127.0.0.1:5000/Authentication/Create_user
     def get(self):
-        return {'Status': 'Working'}, 201
+        return {'Status' : 'Working'}, 201
 
     # curl http://127.0.0.1:5000/Authentication/Create_user -d '{"name":"Inigo", "last_name":"Lopez-Gazpio", "email":"inigo.lopezgazpio@deusto.es"}' -X POST -H "Content-Type: application/json" -v
     def post(self):
         user_args = user_parser.parse_args()
         user = User(user_args.name, user_args.last_name, user_args.email)
         result = deusto_auth.create_user( user )
-        return { 'Result' : result }, 201
+        return {'Result' : result }, 201
 
 
 class Auth_MicroService_Change_Password (Resource):
 
     # curl http://127.0.0.1:5000/Authentication/Change_password
     def get(self):
-        return {'Status': 'Working'}, 201
+        return {'Status' : 'Working'}, 201
 
     # curl http://127.0.0.1:5000/Authentication/Change_password -d '{"email":"inigo.lopezgazpio@deusto.es", "password":"XXX", "password_new":"XXX"}' -X PUT -H "Content-Type: application/json" -v
     def put(self):
         user_args = user_parser.parse_args()
         result = deusto_auth.change_password( user_args.email, user_args.password, user_args.password_new )
-        return { 'Result' : result }, 201
+        return {'Result' : result }, 201
 
 
 class Auth_MicroService_Delete_User (Resource):
 
     # curl http://127.0.0.1:5000/Authentication/Delete_user
     def get(self):
-        return {'Status': 'Working'}, 201
+        return {'Status' : 'Working'}, 201
 
-    # curl http://127.0.0.1:5000/Authentication/Delete_user -d '{"email":"inigo.lopezgazpio@deusto.es", "password":"XXX" }' -X DELETE -H "Content-Type: application/json" -v
-    def delete (self):
+    # curl http://127.0.0.1:5000/Authentication/Delete_user -d '{"email":"inigo.lopezgazpio@deusto.es", "password":"XXX" }' -X PUT -H "Content-Type: application/json" -v
+    def put (self):
         user_args = user_parser.parse_args()
         result = deusto_auth.delete_user(user_args.email, user_args.password)
-        return {'Result': result}, 201
+        return {'Result' : result}, 201
 
 
 api.add_resource(MicroServices, '/')
